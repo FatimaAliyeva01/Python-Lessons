edc5498db29e6273a693ff2d8a393d9d7db3a740
FatimaAliyeva01 <81676388+FatimaAliyeva01@users.noreply.github.com>
2022-01-27 23:13:00 +0400

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+#Data types(Data tipleri)
+# bold text-Numbers -- nomreler
+# -String --- sozler,cumler 
+# -Boolen --- true, false
+# -List--- listler
+# -Tuple
+# -Dictionary --- sozcukler
+# -Set --- dest
+
+#Numbers
+yas = 19
+print=(5+5,yas)
